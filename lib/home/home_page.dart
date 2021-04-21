21696cd492ac60bf3223bfbac6a5510ae6c613e6
Lucas Guimarães Santos <lucasantos@fh.com.br>
2021-04-21 03:19:29 -0300

    home_screen

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:nlw/home/widgets/levelbutton/level_button.dart';
+import 'package:nlw/home/widgets/level_button/level_button.dart';
+import 'package:nlw/home/widgets/quiz_card/quiz_card.dart';
 import 'widgets/appbar/app_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,14 +14,32 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBarWidget(),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 24),
+        child: Column(
           children: [
-            LevelButtonWidget(label: "Fácil"),
-            LevelButtonWidget(label: "Médio"),
-            LevelButtonWidget(label: "Difícil"),
-            LevelButtonWidget(label: "Perito")
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: "Fácil"),
+                LevelButtonWidget(label: "Médio"),
+                LevelButtonWidget(label: "Difícil"),
+                LevelButtonWidget(label: "Perito")
+              ],
+            ),
+            SizedBox(height: 24,),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                crossAxisCount: 2,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
